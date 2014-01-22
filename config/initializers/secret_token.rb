ce5a42811df0cd0fedc5a4f772e7c386ec881402
Chris Frederick <cfreder2@dhcp-10-40-29-57.dhcp.nd.edu>
2014-01-22 12:10:55 -0500

    initial commit based on web marketing team foundation template.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Huginn::Application.config.secret_key_base = 'aeb585802927db723be89a09867845ef8114c702137fb7fda8282abcb2971f7f3b8fdb4aaea5db5551a64729020c1cc6139244d0763d243d95859bb89070ae66'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Huginn::Application.config.session_store :cookie_store, key: '_huginn_session'
