44df2efa3782e0b29ff57636433ed2b9bed019dc
Evan Grantham-Brown <egrantha@nd.edu>
2014-03-21 11:12:43 -0400

    Tweak

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -15,7 +15,6 @@ class TermsController < ApplicationController
     CASClient::Frameworks::Rails::Filter.client.proxy_callback_url =
       "https://data-test.cc.nd.edu:8443/cas_proxy_callback/receive_pgt"
     CASClient::Frameworks::Rails::Filter.filter(self)
-    end
     if session[:cas_pgt]
       logger.debug ":cas_pgt: " + session[:cas_pgt].to_s
     end
