fe26ee89635cf6bfa0e75ee7999d09e3f2008cde
Evan Grantham-Brown <egrantha@nd.edu>
2014-03-28 15:32:09 -0400

    Cleanup

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -3,20 +3,9 @@ require "json"
 require "open-uri"
 
 class TermsController < ApplicationController
-  before_filter :authenticate!, :only => :authenticated_show
-
-  def session_barf
-    session.keys.each do |key|
-      logger.debug "Session key #{key.to_s} => #{session[key].to_s}"
-    end
-  end
-
-  def object_barf(object)
-    object.methods.each do |method|
-      logger.debug method.to_s
-    end
-  end
-
+  #before_filter :authenticate!, :only => :authenticated_show
+  before_filter CASClient::Frameworks::Rails::Filter.filter, :only => :authenticated_show
+  
   def authenticate!
     logger.debug "Authenticating..."
     CASClient::Frameworks::Rails::Filter.client.proxy_callback_url =
@@ -33,37 +22,14 @@ class TermsController < ApplicationController
     muninn_host = Huginn::Application::CONFIG["muninn_host"]
     muninn_port = Huginn::Application::CONFIG["muninn_port"]
 
-    logger.debug "Session at start of authenticated_show method:"
-    session_barf
-
     cas_service_uri = "https://" + muninn_host.to_s + "/"
     proxy_granting_ticket = session[:cas_pgt]
 
     ticket = CASClient::Frameworks::Rails::Filter.client.request_proxy_ticket(proxy_granting_ticket, cas_service_uri)
 
-    logger.debug ticket.to_s
-    if ticket.success != nil
-      logger.debug ticket.success.to_s
-    end
-    if ticket.failure_code != nil
-      logger.debug ticket.failure_code.to_s
-    end
-    if ticket.failure_message != nil
-      logger.debug ticket.failure_message.to_s
-    end
-    if ticket.failure_message != nil
-      logger.debug ticket.ticket.to_s
-    end
-
     logger.debug("Querying Muninn...")
     uri_string = "/terms/" + URI::encode(params[:id])
 
-    #service_uri = muninn_host + uri_string
-    #logger.debug "*** SESSION KEYS ***: " + session.keys.to_s
-    #proxy_granting_ticket = session[:cas_pgt]
-    #logger.debug "*** PROXY GRANTING TICKET ***: " + proxy_granting_ticket
-    #ticket = CASClient::Frameworks::Rails::Filter.client.request_proxy_ticket(service_uri, proxy_granting_ticket).ticket
-    #logger.debug "*** ACTUAL TICKET ***: " + ticket.to_s
     http = Net::HTTP.new(muninn_host, muninn_port)
     http.use_ssl = Huginn::Application::CONFIG["muninn_uses_ssl"]
     if !Huginn::Application::CONFIG["validate_muninn_certificate"]
--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,5 @@ Huginn::Application.initialize!
 
 CASClient::Frameworks::Rails::Filter.configure(
 :cas_base_url => "https://login-test.cc.nd.edu/cas/",
-#:cas_base_url => "https://localhost:8443/cas-server-webapp-3.5.2",
 :proxy_callback_url => "https://data-test.cc.nd.edu/cas_proxy_callback/receive_pgt"
-#:logger => cas_logger
 )
