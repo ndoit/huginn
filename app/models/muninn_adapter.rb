1049e7a1e37226f7525aa51d539adb09d9ceb4bf
Brandon Rich <brich@nd.edu>
2014-06-02 20:31:21 +0000

    added methods for each http action

--- app/models/muninn_adapter.rb
+++ app/models/muninn_adapter.rb
@@ -1,57 +1,59 @@
 class MuninnAdapter
 
-  def self.custom_query(json_string, page, per_page )
-    muninn_host = Huginn::Application::CONFIG["muninn_host"]
-    muninn_port = Huginn::Application::CONFIG["muninn_port"]
-
-    muninn_response = HTTParty.get("http://#{muninn_host}:#{muninn_port}/search/custom/query", { :body => json_string,
-    :headers => { 'Content-Type' => 'application/json'} })
-
-    output_string= ActiveSupport::JSON.decode(muninn_response.body.to_json)
-
-    results= MuninnAdapter.extract_results(output_string)
-
-    results = results.sort_by { |k| "#{k[:sort_name]}"}
-
-    results.paginate(page: page, per_page: per_page)
-
-   end
 
 
 
-  def self.extract_results(search_response)
-    response_hash = JSON.parse(search_response)
-    if !response_hash.has_key?("result")
-      LogTime.info("No contents.")
-      return []
-    end
-    output = []
-    response_hash["result"]["hits"]["hits"].each do |hit|
+  def self.post( resource_uri, body )
+    req = Net::HTTP::Post.new( resource_uri )
+    req["Content-Type"] = "application/json"
+    req.body = body
 
-      node ={
-        :id => hit["_id"].to_i,
-        :type => hit["_type"],
-        :score => hit["_score"],
-        :data => hit["_source"],
-        :sort_name =>hit["_source"]["name"]
-      }
-      if hit["highlight"] != nil
-        if hit["highlight"]["name"] != nil
-          node1  = {:m_name => hit["highlight"]["name"][0]}
-          node.merge!(node1)
-        end
-        if hit["highlight"]["definition"] != nil
-          node2 ={:m_definition => hit["highlight"]["definition"][0]}
-          node.merge!(node2)
-        end
-      end
+    MuninnAdapter.perform( req )
 
-      output << node
+  end
 
+  def self.delete( resource_uri )
+    req = Net::HTTP::Delete.new( resource_uri )
+    req.body = nil
+    MuninnAdapter.perform( req )
+  end
+
+  def self.put( resource_uri, body )
+    req = Net::HTTP::Put.new( resource_uri )
+    req["Content-Type"] = "application/json"
+    req.body = body
+    MuninnAdapter.perform( req )
+  end
+
+
+  def self.get( resource_uri )
+
+    http = MuninnAdapter.new_http_request
+
+    http.use_ssl = Huginn::Application::CONFIG["muninn_uses_ssl"]
+
+    if !Huginn::Application::CONFIG["validate_muninn_certificate"]
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE #for when Muninn is using a self-signed cert
     end
 
-    return output
+    if Huginn::Application::CONFIG["muninn_uses_ssl"]
+      muninn_response = http.get("https://#{muninn_host}:#{muninn_port}/#{resource_uri}")
+    else
+      muninn_response = http.get("http://#{muninn_host}:#{muninn_port}/#{resource_uri}")
+    end
 
+    JSON.parse(muninn_response.body)
+  end
+
+  private
+  def self.new_http_request
+    Net::HTTP.new( Huginn::Application::CONFIG["muninn_host"], Huginn::Application::CONFIG["muninn_port"] )
+  end
+
+  def self.perform( req )
+    response = MuninnAdapter.new_http_request.start do |http|
+      http.request(req)
+    end
   end
 
 
