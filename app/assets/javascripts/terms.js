4724a4e01024d1d96e7cf76b9db508b7b524aa9f
Shelin Mathews <smathews@nd.edu>
2014-06-27 15:52:38 +0000

    added offices edits jquery calls

--- app/assets/javascripts/terms.js
+++ app/assets/javascripts/terms.js
@@ -37,6 +37,30 @@ $(document).ready(function(){
 		})
 
   }
+
+   if(typeof office_detail_json != 'undefined')  {
+
+    $('#updateOfficeButton').click(function() {
+    //alert("updating term object")
+      if (updateOfficeObject(office_detail_json) == false)
+        return false;
+
+      updateOffice(office_detail_json)
+    })
+
+    $('#showOfficeButton').click( function() {
+      $('#json_container').toggle()
+    })
+
+    $('#deleteConfirm').click( function() {
+      $('a.close-reveal-modal').trigger('click')
+      deleteOffice(office_detail_json.id)
+    })
+    $('#deleteCancel').click( function() {
+      $('a.close-reveal-modal').trigger('click')
+    })
+
+  }
   $("#search1").bind("change keyup",function() {
      console.log( $(this) )
      if ($(this).val().length >=2 ) {
@@ -61,6 +85,14 @@ $(document).ready(function(){
      "notes": ""};
     $('a.close-reveal-modal').trigger('click');
     createTerm(term_new);
+  })
+
+$('#addOfficeButton').click(function() {
+  clearValidationErrors()
+  var office = $('#oname').val();
+  office_new = {"name": office};
+    $('a.close-reveal-modal').trigger('click');
+    addOffice(office_new);
   })
 })
 
@@ -114,6 +146,7 @@ function addValidationError( type, message ) {
 function updateTermObject(term_object ) {
 	clearValidationErrors()
 	tinymce.triggerSave();
+  console.log(term_object);
 	$('.editable').each( function() {
 		id = $(this).attr('id');
 		if ( id ) {
@@ -141,46 +174,38 @@ function updateTermObject(term_object ) {
 		stake = $(this).data('raci-stake')
 		console.log("stake value is " + stake);
 		json_array = $('#raci' + i ).select2('data')	
-        if (stake == "Responsible" && json_array.length >1){
-        	addValidationError( "alert", "Only one <b>Responsible</b> office is allowed.")
-        	//alert();
-        	//exitRACI = true;
-        	//return false;
-        }
+    if (stake == "Responsible" && json_array.length >1){
+    	addValidationError( "alert", "Only one <b>Responsible</b> office is allowed.")
+    }
       
 		for (var j = 0; j < json_array.length; j++ ) {
 			term_object["stakeholders"].push( { name: json_array[j]["text"], stake: stake} )
 			var office_exist = false;
-            if (office_array !=null )  {
-            	for (var k=0; k<office_array.length; k++){
-            		if (json_array[j]["text"] == office_array[k]["name"])  {
-            		  office_exist = true;
-            	      break;
-            	    }
-                }
-            }
-
-            if (!office_exist)
-              office_array.push({name: json_array[j]["text"]})	
-            else{
-              if (!office_text)
-                 office_text = json_array[j]["text"]
-              else if (office_text.search( json_array[j]["text"]) <0)
-              	office_text  += " , "+ json_array[j]["text"]
-            }
-
+      if (office_array !=null )  {
+         for (var k=0; k<office_array.length; k++){
+            if (json_array[j]["text"] == office_array[k]["name"])  {
+              office_exist = true;
+              break;
+      	    }
+         }
        }
-       i++;
 
+      if (!office_exist)
+        office_array.push({name: json_array[j]["text"]})	
+      else{
+        if (!office_text)
+          office_text = json_array[j]["text"]
+        else if (office_text.search( json_array[j]["text"]) <0)
+        	office_text  += " , "+ json_array[j]["text"]
+        }
+
+      }
+      i++;
 	}) 
 
   if (office_text !=null)
     addValidationError( "alert", "<b>" +  office_text + "</b>" +  " has repeated in the RACI entry. Each office is assigned for one role per term.");
-      
-		
-	//if (exitRACI) return false;
-	//if (dupRACI) return false;
-	console.log( term_object )
+    console.log( term_object )
 
   if ( showValidationErrors()  == true ) {
     return false;
@@ -234,25 +259,110 @@ function createTerm( term_object ) {
      url : '/terms',
      type: 'POST',
      data: { "term": JSON.stringify(term_object)},
-    // data: { "termJSON": term_object },
      dataType: 'json',
      success: function (data) {
         //alert('term created')
-        addSuccessMessage("success", "<b>Term " + term_object.name +   " successfully. Please wait for Term Detail page display.</b>");
-	    showSuccessMessage();
+       addSuccessMessage("success", "<b>Term " + term_object.name +   " successfully. Please wait for Term Detail page display.</b>");
+	      showSuccessMessage();
         var url = escape('terms/'+ term_object.name);
         //alert(url);
         //$('#term_detail').load(url);
         window.location = url;
      },
      error: function( xhr, ajaxOptions, thrownError) {
-          addValidationError( "alert", "Create term has errors: " + xhr.responseText);
+          addValidationError( "alert", "Added Term, " +term_object.name+ ", has error: " + xhr.responseText);
           showValidationErrors()
 	    }
-  });
-
+  })
 
 }
+
+function addOffice(office_object ) {
+
+  $.ajax({
+     url : '/offices',
+     type: 'POST',
+     data: { "office": JSON.stringify(office_object)},
+     dataType: 'json',
+     success: function (data) {
+        //alert('term created')
+       addSuccessMessage("success", "<b>Office " + office_object.name +   " successfully. Please wait for Term Detail page display.</b>");
+       showSuccessMessage();
+       var url = escape('offices/'+ office_object.name);
+        //alert(url);
+        //$('#term_detail').load(url);
+        window.location = url;
+     },
+     error: function( xhr, ajaxOptions, thrownError) {
+          addValidationError( "alert", "Added office, "+office_object.name+ ", has error: " + jQuery.parseJSON(xhr.responseText).message);
+          showValidationErrors()
+      }
+   })
+
+}
+
+
+function updateOfficeObject(office_object ) {
+  clearValidationErrors()
+  tinymce.triggerSave();
+  console.log(office_object);
+  $('.editable').each( function() {
+    id = $(this).attr('id');
+    if ( id ) {
+      p = tinymce.get(id).getContent()
+      if (id == "name") {
+        var StrippedString = p.replace(/(<([^>]+)>)/ig,"")
+        p = StrippedString;
+      }
+      console.log(p);
+      console.log(id);
+      office_object[id] = p;
+    }
+   
+  });
+
+    return office_object;
+  }
+function updateOffice(office_object  ) {
+
+  $.ajax({
+      url: office_object.id,
+      type: 'PUT',
+      data: {"officeJSON": JSON.stringify(office_object)},
+      dataType: 'json',
+      success: function (data) {
+         var url = escape(office_object.name);
+         addSuccessMessage("success", "<b>" + office_object.name + "</b>" +  " updated successfully. " );
+         showSuccessMessage();
+         window.location = url;
+      },
+      error: function( xhr, ajaxOptions, thrownError) {
+         addValidationError( "alert", "Update Office, <b>" + office_object.name + "</b>  has errors: " + jQuery.parseJSON(xhr.responseText).message);
+         showValidationErrors()
+      }
+  });
+
+}
+
+
+function deleteOffice( officeid ) {
+    $.ajax({
+      url:   officeid,
+      type: 'DELETE',
+      success: function(data, status, xhr){
+        addSuccessMessage("success", "<b>" + data.message + ". Please wait for Glossary Page display.</br>" )
+        showSuccessMessage(); 
+        var url = '../offices'
+        window.location = url;
+      },
+      error: function(xhr, status, error) {
+           //alert(xhr.responseText)
+        addValidationError( "alert", "Delete term has errors: " + jQuery.parseJSON(xhr.responseText).message);
+        showValidationErrors()
+      }
+  });
+}
+
 
 tinymce.init({
     selector: ".editable",
