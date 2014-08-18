5e40bb2a42fdcf49f309d5e12cbb6d16e16655db
Shelin Mathews <smathews@nd.edu>
2014-08-18 18:20:51 +0000

    added CAS hostname

--- config/environment.rb
+++ config/environment.rb
@@ -7,5 +7,5 @@ Huginn::Application.initialize!
 
 CASClient::Frameworks::Rails::Filter.configure(
 :cas_base_url => "https://login.nd.edu/cas/",
-:proxy_callback_url => "https://bitdata1-test.dc.nd.edu/cas_proxy_callback/receive_pgt"
+:proxy_callback_url => "https://data-test.cc.nd.edu/cas_proxy_callback/receive_pgt"
 )
