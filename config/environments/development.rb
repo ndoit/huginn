7c400c602bd0f8959a20acc377114bea023b6bb6
smathewsnd <smathews@nd.edu>
2014-09-18 19:45:25 +0000

    updated proxy call back for dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,5 +27,5 @@ Huginn::Application.configure do
 
   # CAS proxy settings
   config.cas_base_url = "https://login-test.cc.nd.edu/cas/"
-  config.cas_proxy_callback_url = "https://bitdata1-dev.dc.nd.edu/cas_proxy_callback/receive_pgt"
+  config.cas_proxy_callback_url = "https://data-dev.cc.nd.edu/cas_proxy_callback/receive_pgt"
 end
