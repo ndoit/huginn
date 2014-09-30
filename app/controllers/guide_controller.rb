ee8f7ecb7ce8f807e02291c18a5d1fc3ab6865e7
Brandon Rich <brich@nd.edu>
2014-09-30 18:07:10 +0000

    don't show security roles if the person is not logged in

--- app/controllers/guide_controller.rb
+++ app/controllers/guide_controller.rb
@@ -1,40 +1,52 @@
 class GuideController < ApplicationController
 
-  before_filter :node_types
+before_filter :node_types
 
- def node_types
+def node_types
 
-   @node_types = [ 'term', 'report' ]
-   #if current_user
-   # @node_types << "report"
-   #end
+  @node_types = [ 'term', 'report' ]
+  #if current_user
+  # @node_types << "report"
+  #end
 
-   @report_roles = Muninn::SecurityRoleAdapter.all.select { |k| k.report_role? && (current_user.has_role? k.name) }
- end
+  @report_roles = []
+  if current_user 
+    @report_roles = Muninn::SecurityRoleAdapter.all.select { |k| k.report_role? && (current_user.has_role? k.name) }
+  end
+end
 
- def index
+def index
 
- end
+end
 
- def search
-     logger.debug("Querying Muninn...")
+def search
+  logger.debug("Querying Muninn...")
 
-     params[:page] ||= 1
+  params[:page] ||= 1
 
-     mcsa = Muninn::CustomSearchAdapter.new( params )
-     mcsa.filter_reports( current_user.security_roles )
-         .filter_results
+  mcsa = Muninn::CustomSearchAdapter.new( params )  
+  mcsa.filter_reports( role_filter_array )
+      .filter_results
+ 
+  @results = mcsa.results
+  @muninn_result = mcsa.raw_result
+  @selected_node_types = mcsa.selected_node_types  # should the mcsa do this
+  @resource_count_hash = mcsa.resource_count_hash
 
-     @results = mcsa.results
-     @muninn_result = mcsa.raw_result
-     @selected_node_types = mcsa.selected_node_types  # should the mcsa do this
-     @resource_count_hash = mcsa.resource_count_hash
+  if ( params[:page].to_i > 1 )
+    render partial: "partial_search", locals: { results: @results || [] }, layout: false
+  else
+    render html: "search", layout: false
+  end
+end
 
-     if ( params[:page].to_i > 1 )
-       render partial: "partial_search", locals: { results: @results || [] }, layout: false
-     else
-       render html: "search", layout: false
-     end
- end
+private
+  def role_filter_array
+    if current_user 
+      current_user.security_roles
+    else
+      []
+    end
+  end
 
 end
