27ee3e927f8a83a8d3922acea60dbaa799ff2e42
Brandon Rich <brich@nd.edu>
2014-10-20 14:10:18 +0000

    fixed issue where i wasn't passing the netid parameter to muninn

--- app/models/muninn/user_adapter.rb
+++ app/models/muninn/user_adapter.rb
@@ -3,7 +3,7 @@ require "uri"
 class Muninn::UserAdapter
 
 	def self.security_roles( netid )
-		uri = URI.parse("http://#{ENV['muninn_host']}:#{ENV['muninn_port']}/users/#{self.name}/roles")
+		uri = URI.parse("http://#{ENV['muninn_host']}:#{ENV['muninn_port']}/users/#{netid}/roles")
 	    http = Net::HTTP.new( uri.host, uri.port )
 	    response = http.request(Net::HTTP::Get.new( uri.request_uri  ))
 	    json = JSON.parse(response.body)
