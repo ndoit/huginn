a059c5cad0aa4ceb99ebe79eea61ac7f6dd443f6
RyanSnodgrass <ryansnodgrass.dev@gmail.com>
2014-12-17 16:27:41 -0500

    dataset show reading but no CUD

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -1,7 +1,7 @@
 class DatasetsController < ApplicationController
   def show
     logger.debug("Querying Muninn...")
-    dataset_resp = Muninn::Adapter.get( "/datasets/" + URI::encode(params[:id]) )
-    @dataset = JSON.parse(reports_resp.body)
+    datasets_resp = Muninn::Adapter.get( "/datasets/" + URI::encode(params[:id]) )
+    @dataset = JSON.parse(datasets_resp.body)
   end
 end
