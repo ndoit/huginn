846d336570a4cb9168c8127dfc73ef040b302143
RyanSnodgrass <ryansnodgrass.dev@gmail.com>
2015-03-24 18:32:11 +0000

    user security roles now coming from my_access

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,20 @@ require 'json'
 class User
 
   attr_accessor :name, :old_security_roles
-  attr_reader :user_obj, :admin_emeritus, :user_name, :security_roles
+  attr_reader :my_access, :admin_emeritus, :user_name, :security_roles
 
   def initialize( netid, cas_user, cas_pgt )
     @name = netid
     @name ||= "No name found"
-    @user_obj = Muninn::Adapter.get( "/users/" + URI::encode( netid ), cas_user, cas_pgt )
+    @my_access = Muninn::SecurityRoleAdapter.my_access( @name )
   end
 
-  def security_roles
-    @user_obj["security_roles"]
+  def my_roles
+    @my_roles = []
+    @my_access["user"]["roles"].each do |ur|
+      @my_roles << ur[0]
+    end
+    @my_roles
   end
 
   def default_admin_roles
@@ -20,20 +24,42 @@ class User
   end
 
   def has_role?( role )
-    security_roles.include? role
+    @my_roles.include? role
   end
 
-  def admin_emeritus?
-    @user_obj["user"]["admin_emeritus"]
+  def is_admin?
+    @my_access["user"]["is_admin"]
   end
+
+  def list_create_access
+    @list_create_access = @my_access["user"]["create_access_to"]
+  end
+
+  def has_create_access_to( node_type )
+    list_create_access.include? node_type
+  end
+
+  def list_read_access
+    @list_read_access = @my_access["user"]["read_access_to"]
+  end
+
+  def has_read_access_to( node_type )
+    list_read_access.include? node_type
+  end
+
+
+  ## Currently my_access does not return admin_emeritus field
+  # def admin_emeritus?
+  #   @my_access["user"]["admin_emeritus"]
+  # end
 
   def user_name
-    @user_obj["user"]["net_id"]
+    @my_access["user"]["net_id"]
   end
 
-  def old_security_roles
-    @roles ||= get_security_roles
-  end
+  # def old_security_roles
+  #   @roles ||= get_security_roles
+  # end
 
   def can_edit_any?
     @can_edit_any = false
@@ -46,14 +72,19 @@ class User
   end
 
   def can( action )
-    Services::Permissions.can( old_security_roles, action )
+    case action
+    when :publish_report
+      has_create_access_to( "report" ) 
+    when :edit_term
+      has_create_access_to( "term" )
+    end
   end
 
-  private
+  # private
 
-  def get_security_roles
-    # call web service
-    Muninn::UserAdapter.security_roles( self.name )
-  end
+  # def get_security_roles
+  #   # call web service
+  #   Muninn::UserAdapter.security_roles( self.name )
+  # end
 
 end
