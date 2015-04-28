4ad5062225b9b4e75051f27ac63c9fc837e5e6f1
RyanSnodgrass <ryansnodgrass.dev@gmail.com>
2015-04-28 14:47:14 +0000

    photo tests passing

--- spec/models/photo_mapper_spec.rb
+++ spec/models/photo_mapper_spec.rb
@@ -4,23 +4,6 @@ require 'carrierwave/test/matchers'
 describe PhotoMapper do
   include CarrierWave::Test::Matchers
 
-  # before(:all) do
-  #   # @saved_photo = build(:photo_mapper)
-  #   # puts @photo.image_uploader.small.read
-    
-    # @saved_photo = PhotoMapper.new(1)
-    # @saved_photo.uploader = File.open('app/assets/images/thug_dog2.jpg')
-    # @saved_photo.save
-
-
-    # @url = URI(@saved_photo.image_url(:thumb))
-  # end
-
-  # after(:all) do
-  #   @saved_photo.uploader.remove!
-  # end
-
-
   context 'Base PhotoMapper' do
     it 'should be of class PhotoMapper' do
       @saved_photo = PhotoMapper.new("Super cool Report")
@@ -32,7 +15,6 @@ describe PhotoMapper do
   end
 
   context 'Saved PhotoMapper' do
-
     it 'saved an image on S3' do
       @saved_photo = PhotoMapper.new(1)
       @saved_photo.uploader = File.open('app/assets/images/thug_dog2.jpg')
@@ -46,69 +28,68 @@ describe PhotoMapper do
       @saved_photo.uploader = File.open('app/assets/images/thug_dog2.jpg')
       @saved_photo.save
       @url = URI(@saved_photo.image_url(:thumb))
-
       @saved_photo.uploader.remove!
-
       response = Net::HTTP.get_response(@url)
       expect(response.code).to eql('403')
     end
   end
 
   context 'current_time is same between both files' do
-    it 'current_time is in ImageUploader class' do
+    it 'should have a current_time method return an integer close to seconds' do
       @saved_photo = PhotoMapper.new(1)
       @saved_photo.uploader = File.open('app/assets/images/thug_dog2.jpg')
-      expect(@saved_photo.uploader.filename).to include(Time.now.to_i.to_s)
+      expect(@saved_photo.uploader.current_time.to_s.length).to match(10)
+    end
+    it 'time is in ImageUploader class' do
+      @saved_photo = PhotoMapper.new(1)
+      @saved_photo.uploader = File.open('app/assets/images/thug_dog2.jpg')
+      expect(@saved_photo.uploader.filename).to include(@saved_photo.uploader.current_time.to_s)
+    end
+    it 'should have the modified time in the full filename' do
+      @saved_photo = PhotoMapper.new(1)
+      @saved_photo.uploader = File.open('app/assets/images/thug_dog2.jpg')
+      # @saved_photo.save
+      current_time = Time.now.to_i
+      expect(@saved_photo.filename(:small)).to include(@saved_photo.uploader.current_time.to_s)
     end
   end
-
-
 
   context 'The GET image_url' do
     it 'should include the matching upload path' do
       @saved_photo = PhotoMapper.new(1)
       @saved_photo.uploader = File.open('app/assets/images/thug_dog2.jpg')
-      @saved_photo.save
+      # @saved_photo.save
       expect(@saved_photo.image_url(:small)).to include(@saved_photo.uploader.store_dir)
     end
-    it 'should have the modified time in the filename' do
-      @saved_photo = PhotoMapper.new(1)
-      @saved_photo.uploader = File.open('app/assets/images/thug_dog2.jpg')
-      @saved_photo.save
-      current_time = Time.now.to_i
-      expect(@saved_photo.filename(:small)).to include(current_time.to_s)
-    end
-    xit 'should explicitly match this line with a name as a string' do
+    it 'should explicitly match this line with a name as a string' do
       @saved_photo = PhotoMapper.new("Cool Report")
       @saved_photo.uploader = File.open('app/assets/images/thug_dog2.jpg')
-      @saved_photo.save
+      # @saved_photo.save
       current_time = Time.now.to_i
-      expect(@saved_photo.image_url(:thumb)).to match("https://s3.amazonaws.com/bi-portal.dcnd/uploads/#{Rails.env}/photo_mapper/Cool%20Report/thumb_#{current_time}_Cool%20Report.png")
+      expect(@saved_photo.image_url(:thumb)).to match("https://s3.amazonaws.com/bi-portal.dcnd/uploads/#{Rails.env}/photo_mapper/Cool%20Report/thumb_Cool%20Report_#{@saved_photo.uploader.current_time}.png")
     end
-    xit 'should explicitly match this line with a name as an integer' do
+    it 'should explicitly match this line with a name as an integer' do
       @saved_photo = PhotoMapper.new(1)
       @saved_photo.uploader = File.open('app/assets/images/thug_dog2.jpg')
-      @saved_photo.save
+      # @saved_photo.save
       current_time = Time.now.to_i
-      expect(@saved_photo.image_url(:thumb)).to match("https://s3.amazonaws.com/bi-portal.dcnd/uploads/#{Rails.env}/photo_mapper/1/thumb_#{current_time}_1.png")
+      expect(@saved_photo.image_url(:thumb)).to match("https://s3.amazonaws.com/bi-portal.dcnd/uploads/#{Rails.env}/photo_mapper/1/thumb_1_#{@saved_photo.uploader.current_time}.png")
     end
   end
   
-
-  context 'image resizing' do
-  
-    xit "should scale down a landscape image to be exactly 75 by 75 pixels" do 
-      # @test_photo = PhotoMapper.new(1)
-      # @test_photo.uploader.store!(File.open('app/assets/images/old_lhamascarf.jpg'))
-      expect(@saved_photo.image_url(:thumb)).to have_dimensions(75, 75)
-    end
-    xit "should scale down a landscape image to fit within 200 by 200 pixels" do
-      string = @saved_photo.uploader.small.read.path
-      string.slice! "/vagrant/apps/huginn"
-      CarrierWave::Test::Matchers::ImageLoader.load_image(string)
-      expect(@photo.uploader.small.read).to be_no_larger_than(200, 200)
-    end
-  end
+  # context 'image resizing' do
+  #   xit "should scale down a landscape image to be exactly 75 by 75 pixels" do 
+  #     # @test_photo = PhotoMapper.new(1)
+  #     # @test_photo.uploader.store!(File.open('app/assets/images/old_lhamascarf.jpg'))
+  #     expect(@saved_photo.image_url(:thumb)).to have_dimensions(75, 75)
+  #   end
+  #   xit "should scale down a landscape image to fit within 200 by 200 pixels" do
+  #     string = @saved_photo.uploader.small.read.path
+  #     string.slice! "/vagrant/apps/huginn"
+  #     CarrierWave::Test::Matchers::ImageLoader.load_image(string)
+  #     expect(@photo.uploader.small.read).to be_no_larger_than(200, 200)
+  #   end
+  # end
 
 
 
