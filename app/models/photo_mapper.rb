2caf4f6adb478d91fbef3e0d18b512b94f657fa5
Evan <egrantha@nd.edu>
2015-05-05 16:23:58 -0400

    Fixing photomapper merge error.

--- app/models/photo_mapper.rb
+++ app/models/photo_mapper.rb
@@ -1,36 +1,39 @@
 # for now, this class only exists to contain the CarrierWave uploader
 
-
 class PhotoMapper
-
   extend CarrierWave::Mount
-  attr_accessor :id, :uploader
+  attr_accessor :name, :uploader
   mount_uploader :uploader, ImageUploader
-  ## should probably rename :report_image to something that describes its behavior
-  ## perhaps something like :accept_image
-  ## because in the console to get the image into the ReportPhoto class =>
-    ## 2.0.0-p353 :003 > r.report_image = File.open('app/assets/images/thug_dog.jpg')
-    ##   => #<File:app/assets/images/thug_dog.jpg>
 
-  def initialize( id )
-    @id = id
+  def initialize( name, timestamp = nil)
+    @name = name
+    unless timestamp.present?
+      @timestamp ||= Time.now.to_i.to_s
+    else
+      @timestamp = timestamp.to_s
+    end
   end
 
   def save
       self.store_uploader!
   end
 
-  def image_url( size )  
+  def image_url
     # xxxx/report_photo/xxxx is where the problem for getting was
     # by having 'model.class.to_s' the file path is dependent on the model
     # when GETting the images, they wont forever be a 'report'
     # so might want to refactor this in the future
-    url = "/uploads/#{Rails.env}/photo_mapper/#{self.id.to_s}/#{filename(size)}"
+    url = "/uploads/#{Rails.env}/photo_mapper/#{URI.escape(self.name.to_s)}/#{filename}"
     root + url
   end
 
-  def filename(size)
-    size.to_s + "_" + self.id.to_s + ".png"
+  def timestamp
+    @timestamp
+  end
+
+  def filename
+    # @timestamp = Time.now.to_i.to_s
+    URI.escape(self.name.to_s) + "_" + @timestamp + ".png"
   end
 
   def root
