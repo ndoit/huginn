362667bcfdf543db4e69210f4c18fcafcf229438
RyanSnodgrass <ryansnodgrass.dev@gmail.com>
2015-06-01 11:51:29 -0400

    fall back to assets pipeline set to true

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -27,7 +27,7 @@ Huginn::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
   # config.assets.compile = true
   config.assets.precompile =  []
 
