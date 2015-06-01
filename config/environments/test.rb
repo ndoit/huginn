2097e3d43e9ff8c4e22abf4b0af84d76bc8cbaa5
Ryan Snodgrass <ryansnodgrass.dev@gmail.com>
2015-06-01 10:38:01 -0400

    Update test.rb

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -29,7 +29,7 @@ Huginn::Application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
   # config.assets.compile = true
-  config.assets.precompile =  ['*.js', '*.css', '*.css.erb']
+  config.assets.precompile =  ['*.js']
 
   # Generate digests for assets URLs.
   config.assets.digest = true
