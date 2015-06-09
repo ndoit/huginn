231d38f113bf267e97e8fbed0cf1faa8b9ab2fad
Shelin Mathews <smathews@nd.edu>
2015-06-09 14:57:07 -0400

    host header attack fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
 
   # before_filter CASClient::Frameworks::Rails::Filter
   
@@ -30,6 +31,13 @@ class ApplicationController < ActionController::Base
   end
 
   helper_method :current_user
+
+  private
+    def debug_headers
+        if request.env['HTTP_X_FORWARDED_HOST']
+            request.env.except!('HTTP_X_FORWARDED_HOST') # just drop the variable
+        end
+    end # def
 
 
 
